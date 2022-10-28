d561ac7dcc2ba0bdf873491541e63662f5e28c4a
LAPTOP-9KQD6BO4\s_moe <s.moeen.kamali@gmail.com>
2022-10-28 19:15:59 +0330

    finished up

--- boot/boot.asm
+++ boot/boot.asm
@@ -4,9 +4,25 @@
 section code
 
 .switch:
+    mov ax, 0x4f01 ; querying the VBE
+    mov cx, 0x111 ; Mode we want
+    mov bx, 0x0800 ; Offset for the vbe info structure
+    mov es, bx
+    mov di, 0x00
+    int 0x10
+
+    ; Make the switch to graphics mode
+    mov ax, 0x4f02
+    mov bx, 0x111
+    int 0x10
+
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+
     mov bx, 0x1000 ; This is the location where the code is loaded from hard disk
     mov ah, 0x02
-    mov al, 30 ; The number of sectors to read from hard disk
+    mov al, 50 ; The number of sectors to read from hard disk
     mov ch, 0x00
     mov dh, 0x00
     mov cl, 0x02
@@ -44,14 +60,14 @@ gdt_null_descriptor:
     dd 0x00
     dd 0x00
 gdt_code_seg:
-    dw 0xffff
+    dw 0xeeee
     dw 0x00
     db 0x00
     db 10011010b
     db 11001111b
     db 0x00
 gdt_data_seg:
-    dw 0xffff
+    dw 0xeeee
     dw 0x00
     db 0x00
     db 10010010b
--- makefile
+++ makefile
@@ -1,11 +1,10 @@
 all: bootloader
 
 bootloader:
-	
-	nasm D:\Projects\Active\DreamsOS\boot\boot.asm -f bin -o boot\bin\boot.bin
+	nasm boot\boot.asm -f bin -o boot\bin\boot.bin
 	nasm boot\kernel_entry.asm -f elf -o boot\bin\kernel_entry.bin
 	
-	gcc -m32 -ffreestanding -c boot\main.c -o boot\bin\kernel.o
+	gcc -m32 -ffreestanding -c boot\final.c -o boot\bin\kernel.o
 	ld -m elf_i386 -o boot\bin\kernel.img -Ttext 0x1000 boot\bin\kernel_entry.bin boot\bin\kernel.o
 
 	objcopy -O binary -j .text boot\bin\kernel.img boot\bin\kernel.bin
@@ -16,5 +15,3 @@ clear:
 
 run:
 	C:\Program Files\qemu\qemu-system-x86_64.exe -L "C:\Program Files\qemu" -drive format=raw,file=os.img
-
- 
